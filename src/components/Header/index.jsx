c2859dbfb3721053fc09df03902689fa18cfc327
rafael <rafael.penner465@gmail.com>
2023-11-20 17:09:27 -0300

    authorization

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,16 @@
 import { InputSearch } from '../../components/InputSearch';
 import { FiMenu } from 'react-icons/fi';
+
 import { useAuth } from '../../hooks/auth';
+import { USER_ROLE } from '../../utils/roles';
+
 import { PiReceipt } from "react-icons/pi";
 
 import { Container, Nav, Notification } from "./styles";
 
 export function Header() {
-    const { signOut } = useAuth();
+
+    const { signOut, user } = useAuth();
 
     return (
         <Container>
@@ -25,9 +29,17 @@ export function Header() {
                 logout
             </button>
 
-            <div className='search'>
-                <InputSearch />
-            </div>
+            {
+                //[USER_ROLE.ADMIN].includes(user.role) && 
+                // por exemplo: {user.role === USER_ROLE.ADMIN && <div className='search'>}
+                
+                user.role === USER_ROLE.ADMIN && //se eu colocar essa linha na frenta do codigo dentro de chaves posso separa linhas especificas de quem acessa
+                <>
+                    <div className='search'>   
+                        <InputSearch />
+                    </div>
+                </>
+            }
 
             <Notification>
                 <PiReceipt />
