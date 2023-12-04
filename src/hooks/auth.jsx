93f255409412dac9d5a4c15a6a9b6150e432c34f
rafael <rafael.penner465@gmail.com>
2023-12-04 04:06:03 -0300

    update

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -6,6 +6,7 @@ export const AuthContext = createContext({});
 
 function AuthProvider({ children }) {
     const [data, setData] = useState({});
+    const navigate = useNavigate();
 
     async function signIn({ email, password }) {
 
